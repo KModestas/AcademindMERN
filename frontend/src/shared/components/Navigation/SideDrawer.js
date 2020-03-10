e9c7955cc05dfa3d9c4741689863494aaec77905
Modestas <modestask97@gmail.com>
2020-03-10 15:26:56 +0000

    drawer-hook html element

--- frontend/src/shared/components/Navigation/SideDrawer.js
+++ frontend/src/shared/components/Navigation/SideDrawer.js
@@ -1,12 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-import './SideDrawer.css';
+import './SideDrawer.css'
 
 const SideDrawer = props => {
-  const content = <aside className="side-drawer">{props.children}</aside>;
+	const content = <aside className='side-drawer'>{props.children}</aside>
 
-  return ReactDOM.createPortal(content, document.getElementById('drawer-hook'));
-};
+	// A react portal allows you to render React elements in other areas of the HTML page aside from the 'root' element
+	return ReactDOM.createPortal(content, document.getElementById('drawer-hook'))
+}
 
-export default SideDrawer;
+export default SideDrawer
