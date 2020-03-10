48e63d368e008aa95fe93c164e102943f640314e
Modestas <modestask97@gmail.com>
2020-03-10 15:36:31 +0000

    backdrop for sidedrawer

--- frontend/src/shared/components/Navigation/SideDrawer.js
+++ frontend/src/shared/components/Navigation/SideDrawer.js
@@ -1,13 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-import './SideDrawer.css'
+import './SideDrawer.css';
 
 const SideDrawer = props => {
-	const content = <aside className='side-drawer'>{props.children}</aside>
+  const content = <aside className="side-drawer">{props.children}</aside>;
 
-	// A react portal allows you to render React elements in other areas of the HTML page aside from the 'root' element
-	return ReactDOM.createPortal(content, document.getElementById('drawer-hook'))
-}
+  return ReactDOM.createPortal(content, document.getElementById('drawer-hook'));
+};
 
-export default SideDrawer
+export default SideDrawer;
