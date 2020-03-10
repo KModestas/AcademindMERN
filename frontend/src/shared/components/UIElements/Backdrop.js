ea12f54f6e76c60feb14a18d2bb1fec16d53a1e6
Modestas <modestask97@gmail.com>
2020-03-10 15:51:47 +0000

    side drawer animation

--- frontend/src/shared/components/UIElements/Backdrop.js
+++ frontend/src/shared/components/UIElements/Backdrop.js
@@ -1,14 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-import './Backdrop.css'
+import './Backdrop.css';
 
-// this is a background rendered behind the side drawer, allowing you to click and close the drawer
 const Backdrop = props => {
-	return ReactDOM.createPortal(
-		<div className='backdrop' onClick={props.onClick}></div>,
-		document.getElementById('backdrop-hook')
-	)
-}
+  return ReactDOM.createPortal(
+    <div className="backdrop" onClick={props.onClick}></div>,
+    document.getElementById('backdrop-hook')
+  );
+};
 
-export default Backdrop
+export default Backdrop;
