3d030d193f58727b5f44c294d1785955f691aec0
Modestas <modestask97@gmail.com>
2020-05-16 13:09:51 +0100

    updating and deleting places

--- backend/backend/app.js
+++ backend/backend/app.js
@@ -14,22 +14,24 @@ app.use('/api/places', placesRoutes); // => /api/places...
 app.use('/api/users', usersRoutes);
 
 app.use((req, res, next) => {
-	const error = new HttpError('Could not find this route.', 404);
-	throw error;
+  const error = new HttpError('Could not find this route.', 404);
+  throw error;
 });
 
 app.use((error, req, res, next) => {
-	if (res.headerSent) {
-		return next(error);
-	}
-	res.status(error.code || 500);
-	res.json({ message: error.message || 'An unknown error occurred!' });
+  if (res.headerSent) {
+    return next(error);
+  }
+  res.status(error.code || 500);
+  res.json({ message: error.message || 'An unknown error occurred!' });
 });
 
 mongoose
-	.connect(
-		'mongodb+srv://modestas:mongo123@cluster0-c21gx.gcp.mongodb.net/test?retryWrites=true&w=majority',
-		{ useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }
-	)
-	.then(() => app.listen(5000))
-	.catch((err) => console.log(err));
+  .connect('mongodb+srv://manu:academind123@cluster0-ntrwp.mongodb.net/places?retryWrites=true&w=majority')
+  .then(() => {
+    app.listen(5000);
+  })
+  .catch(err => {
+    console.log(err);
+  });
+
